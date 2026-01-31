7cd36aa50bd9e7846455a1fc0ef90afb418520a6
Aryan Sharma <indiaryan100@gmail.com>
2026-01-31 12:44:49 +0530

    Create automated tests for formatCurrency

---
+++ tests/moneyTests.js
@@ -0,0 +1,27 @@
+import {formatCurrency}  from "../js/utilis/money.js";
+console.log('test suite:formatCurrency');
+
+console.log('Converts cents into dollars');
+if(formatCurrency(2095)==='20.95'){
+    console.log('passed');
+}
+else{
+    console.log('failed');
+}
+
+console.log('Works with Zero');
+if(formatCurrency(0)==='0.00'){
+    console.log('passed');
+}
+else{
+    console.log('failed');
+}
+
+console.log('Round  up to nearest centt');
+if(formatCurrency(2095)==='20.95'){
+    console.log('passed');
+}
+else{
+    console.log('failed');
+}
+
